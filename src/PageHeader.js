c1e727949e4d1703355699b2082d5bdf791f8b42
jamiejamiebobamie <jmccrory@vt.edu>
2019-08-27 17:26:22 -0700

    added code

---
+++ src/PageHeader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './PageHeader.css'
+
+
+function PageHeader() {
+  return (
+    <div className="PageHeader">
+      <header>
+        <h1>SF Public Spaces</h1>
+      </header>
+    </div>
+  )
+}
+
+export default PageHeader
